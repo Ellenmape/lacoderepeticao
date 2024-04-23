1949ebc883ac19095051a492326453581527c64a
unknown <ellenmayarap@gmail.com>
2024-04-23 20:48:10 -0300

    commit

--- src/lacosderepeticao/DoWhileEx05.java
+++ src/lacosderepeticao/DoWhileEx05.java
@@ -4,21 +4,21 @@ import java.util.Scanner;
 
 public class DoWhileEx05 {
 
-	public static void main(String[] args) { 
- int somaNumeros= 0;
-	int numeroAtual = 0;
-	      Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		int somaNumeros = 0;
+		int numeroAtual = 0;
+		Scanner scanner = new Scanner(System.in);
 
-    do {
-        System.out.println("Digite um número: ");
-        numeroAtual = scanner.nextInt();
- 
-        if(numeroAtual >= 0) {
-     	   somaNumeros = numeroAtual + somaNumeros;          	   
-        }
+		do {
+			System.out.println("Digite um número: ");
+			numeroAtual = scanner.nextInt();
 
- } while (numeroAtual != 0);
-    System.out.println("A soma dos números positivos é:: " + somaNumeros);
-}
+			if (numeroAtual >= 0) {
+				somaNumeros = numeroAtual + somaNumeros;
+			}
+
+		} while (numeroAtual != 0);
+		System.out.println("A soma dos números positivos é:: " + somaNumeros);
+	}
 
 }
